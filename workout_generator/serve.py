f83b4f7a455890b9006cf435615a1c3bca201d1e
dsubnet0 <dsubnet0@gmail.com>
2021-01-31 13:08:17 -0500

    Final touches for Flash app-ness

--- workout_generator/serve.py
+++ workout_generator/serve.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from .main import generate_workout,stringify_workout
 app = Flask(__name__)
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def standard_workout():
     return stringify_workout(generate_workout(10))
+
+@app.route('/workout')
+def formatted_workout(workout_array=None):
+    return render_template('workout.html', workout_array=generate_workout(11))
--- workout_generator/serve.sh
+++ workout_generator/serve.sh
@@ -1,2 +1,2 @@
 export FLASK_APP=workout_generator.serve.py
-flask run
+flask run --host=192.168.86.33

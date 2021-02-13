8812545f5e4a806b9b9e7386ca7de2baef1ca423
dsubnet0 <dsubnet0@gmail.com>
2021-02-13 12:31:27 -0500

    Fix font sizing. Start of providing multiple endpoints.

--- serve.py
+++ serve.py
@@ -4,12 +4,22 @@ from flask import Flask, render_template
 from main import generate_workout,stringify_workout
 app = Flask('Doug''s Workout Generator')
 
-@app.route('/')
-def standard_workout():
-    return stringify_workout(generate_workout(10))
+@app.route('/raw')
+@app.route('/raw/<int:number_of_rounds>')
+def standard_workout(number_of_rounds=18):
+    return stringify_workout(generate_workout(number_of_rounds))
 
 @app.route('/workout')
-def formatted_workout(workout_array=None):
-    return render_template('workout.html', workout_array=generate_workout(18))
+@app.route('/workout/<int:number_of_rounds>')
+def formatted_workout(number_of_rounds=18):
+    return render_template('workout.html', workout_array=generate_workout(number_of_rounds))
+
+@app.route('/quietworkout')
+@app.route('/quietworkout/<int:number_of_rounds>')
+def formatted_quiet_workout(number_of_rounds=18):
+    return render_template(
+            'workout.html', 
+            workout_array=generate_workout(number_of_rounds, quietable_only=True)
+            )
 
 app.run(host='0.0.0.0', port=int(os.environ.get("PORT",5000)), debug=True)

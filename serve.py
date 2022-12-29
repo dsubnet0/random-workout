e6ff41ce95b9cb252a6190b2125d0840567147d6
dsubnet0 <dsubnet0@gmail.com>
2022-12-29 18:44:46 +0000

    Hard-code absolute path for PA

--- serve.py
+++ serve.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 
 from main import generate_workout, stringify_workout
 
-app = Flask('Doug''s Workout Generator')
+app = Flask('Doug''s Workout Generator', template_folder='/home/dsubnet0/random-workout/templates')
 
 @app.route('/raw')
 @app.route('/raw/<int:number_of_rounds>')
@@ -47,5 +47,5 @@ def add_move():
         is_opener = userdata['is_opener']
 
 
-
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT",5000)), debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)), debug=True)
